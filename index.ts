cf3b6604a6c8dbd0f998294977d061d326dec332
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-03 21:18:13 +0800

    updated build command

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env tsx
 import chalk from "chalk";
+import pkg from "./package.json"
 
 function main_menu() {
     console.log(
         chalk.green("McRScaffolder") +
-        chalk.dim(` v${process.env.npm_package_version}`))
+        chalk.dim(` v${pkg.version}`))
 
 }
 
