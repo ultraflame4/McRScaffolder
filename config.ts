ed06e77315e35860fc21cc36ae176c3ceae12032
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-04 10:52:29 +0800

    did some stuff

--- config.ts
+++ config.ts
@@ -2,10 +2,11 @@ import * as esbuild from 'esbuild'
 
 await esbuild.build({
     entryPoints: ['src/index.ts'],
-    bundle: true,
+    bundle: false,
     outdir:"bin",
     allowOverwrite: true,
     format: "esm",
     platform: "node",
-    packages: "external"
+    packages: "external",
+    splitting: true
 })
