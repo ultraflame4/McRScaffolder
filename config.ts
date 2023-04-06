ff75500104d092b4168958ae47173b6cadab6230
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-06 16:01:12 +0800

    fixed build config

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild'
 
 await esbuild.build({
     entryPoints: ['src/index.ts'],
-    bundle: false,
+    bundle: true,
     outdir:"bin",
     allowOverwrite: true,
     format: "esm",
