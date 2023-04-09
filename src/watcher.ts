d472dd8129829b17375913cd4841681837227a66
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-09 12:54:39 +0800

    did mor things

--- src/watcher.ts
+++ src/watcher.ts
@@ -2,6 +2,10 @@ import {Project} from "./project";
 import chokidar from "chokidar"
 import ora from "ora"
 import chalk from "chalk";
+import fs from "fs";
+import path from "path";
+import {program} from "commander";
+import * as readline from "readline";
 
 
 
@@ -20,12 +24,21 @@ class Watcher{
         this.logRaw(chalk.yellowBright("~"),`Modified ${type_}`,path)
     }
     private logRemove(path:string,type_:string) {
-        this.logRaw(chalk.redBright("-"),`Removed ${type_}`,path)
+        this.logRaw(chalk.redBright("-"),`Removed ${type_}`,this.resolveDest(path))
+    }
+
+    private resolveDest(path_: string) {
+        return path.relative(path_,Project.resolve(Project.config.pack_name))
     }
     public run() {
         const proj_pack_path = Project.resolve(Project.config.pack_name)
 
-        this.spinner.start(`Watching ${proj_pack_path} for changes...`)
+        this.spinner.start([
+            "Watching",
+            proj_pack_path,
+            "for changes...",
+            chalk.dim("(Ctrl+C to quit)")
+        ].join(" "))
 
         const watcher = chokidar.watch(proj_pack_path,{
             awaitWriteFinish: {
@@ -36,6 +49,7 @@ class Watcher{
 
         watcher.on("add", (path)=>{
             this.logAdd(path,"file")
+
         });
         watcher.on("change", (path)=>{
            this.logModfied(path,"file")
@@ -54,8 +68,19 @@ class Watcher{
         watcher.on("error", (e)=>{
             console.log("\n"+chalk.redBright(`x Error -`), e)
             watcher.close().then(value => this.spinner.fail("Error occured while file watching. Watch stopped."))
-
         })
+
+        process.on('exit',()=>{
+            this.spinner.fail("Stopped because process exited")
+        })
+
+        readline.emitKeypressEvents(process.stdin)
+        process.stdin.on("keypress",(ch,key)=>{
+            if (key && key.ctrl && key.name === "c") {
+                process.exit()
+            }
+        })
+        process.stdin.setRawMode(true)
     }
 }
 
