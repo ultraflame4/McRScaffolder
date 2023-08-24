e4e98b666e9663fbb372f94024c1e8f3da10e3b8
ultr42 <ultr42@skiff.com>
2023-08-24 12:02:28 +0800

    Queries starting with a colon ':' will now omit search results (and only show commands)

--- src/prompts/searchlist.ts
+++ src/prompts/searchlist.ts
@@ -90,10 +90,15 @@ export const SearchList = createPrompt<ISearchListChoice, ISearchListOptions>((c
     const [query, setQuery] = useState("")
     const prefix = usePrefix()
 
-    const filteredChoices = fuzzySearch(query, config.choices)
+
+    const searchResults = fuzzySearch(query, config.choices)
+    const isCommand = query.charAt(0) == ":"
+    const filteredChoices = isCommand ? [] : searchResults
     if (config.allowCancel) {
-        filteredChoices.push({text:"Quit", id:cancelChoiceId})
+        filteredChoices.push({text: "Quit / Exit", id: cancelChoiceId})
     }
+
+
     const choice = filteredChoices[cursorPosition] as ISearchListChoice;
 
     if (isComplete) {
