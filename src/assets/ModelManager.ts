6504f0e2bd1d53882e3eac2583d6e5d78194c580
ultr42 <ultr42@skiff.com>
2023-08-26 10:18:40 +0800

    Revert "I GIVE UP TYPESCRIPT IS SO BADD FOR BUILDING GOOD CODE STRUCTURES"
    This reverts commit df562a259672c1f71a81fe4705471e086aef8f6a.

--- src/assets/ModelManager.ts
+++ src/assets/ModelManager.ts
@@ -3,26 +3,21 @@ import {Project_} from "../core/project";
 import path from "path";
 import fs from "fs";
 import SummaryManager from "../resources/SummaryManager";
-import {ReadJson} from "../core/tools";
 
 const RESOURCE_CTX = "models"
 const RESOURCE_EXT = ".json"
 
-export class WritableAssetModel extends AssetModel{
-
-    public static FromExisting(asset_model:AssetModel) {
-        return  new WritableAssetModel(
-            asset_model.model_id,
-            asset_model.parent,
-            structuredClone(asset_model.textures),
-        )
-    }
-}
 
 /**
  * Independent class managing minecraft models
  */
 export class AssetModelManager{
+    private project: Project_;
+
+
+    constructor(project_: Project_) {
+        this.project = project_;
+    }
 
     public ExistsModelInProject(model_id: ResourceName):boolean {
         let model_path = model_id.filepath(RESOURCE_CTX,RESOURCE_EXT)
@@ -35,21 +30,8 @@ export class AssetModelManager{
     public async GetModelFromProject(model_id: ResourceName): Promise<AssetModel> {
         // First check if model exists in project
         if (this.ExistsModelInProject(model_id)) {
-            try{
-                let json = await ReadJson(model_id.filepath(RESOURCE_CTX,RESOURCE_EXT))
-                return new AssetModel(
-                    model_id,
-                    json.parent as string,
-
-                )
-            }
-            catch (e) {
-
-            }
-
+            return await SummaryManager.read_model(model_id)
         }
-        // Else return default model
-        return await SummaryManager.read_model(model_id)
     }
 
 
