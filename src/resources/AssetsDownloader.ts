23d8ea4b8886cb9b137f6ff374f1e708ca76379d
ultr42 <ultr42@skiff.com>
2023-08-26 20:03:22 +0800

    some changes to AssetDownloader.ts

--- src/resources/AssetsDownloader.ts
+++ src/resources/AssetsDownloader.ts
@@ -2,27 +2,45 @@ import {ResourceName} from "../core/types";
 import {DownloadFile, GetVersionTag} from "../core/tools";
 import {Project} from "../core/project";
 import path from "path";
+import fs from "fs";
 
-class AssetsDownloader{
+
+export interface AssetDownloadResult{
+    path: string,
+    status: "success" | "existing" | "error"
+}
+
+class AssetsDownloader {
     /**
      * Downloads 1 or more assets into the resource pack given the resource names , context and file extension
-     * @param names Resource names
+     * @param name
      * @param ctx The context, eg. "textures"
      * @param ext The file extension with the . eg. ".png"
-     * @returns Returns a collection of resource names and their respective download path
+     * @param overwrite Whether to overwrite existing file. Defaults to false
+     * @returns Returns
      */
-    public async downloadAsset(names: ResourceName[],ctx:string,ext:string=null): Promise<Array<{path:string,resource:ResourceName}>> {
-        let download_paths:Array<{path:string,resource:ResourceName}> = []
-        for (let i = 0; i < names.length; i++) {
-            let value = names[i]
-            const dl_path = path.join(`misode/mcmeta/${GetVersionTag(Project.config.version_id,"assets")}`,value.rel_path(ctx)+(ext??""))
-            const dl_link = new URL(dl_path,"https://raw.githubusercontent.com")
-            download_paths.push({
-                path: await DownloadFile(dl_link.toString(),value.filepath(ctx,ext)),
-                resource: value
-            })
+    public async downloadAsset(name: ResourceName, ctx: string, ext: string = null, overwrite: boolean = false): Promise<AssetDownloadResult> {
+        const dl_path = path.join(`misode/mcmeta/${GetVersionTag(Project.config.version_id, "assets")}`, name.rel_path(ctx) + (ext ?? ""))
+        const dl_link = new URL(dl_path, "https://raw.githubusercontent.com")
+
+        let status: AssetDownloadResult["status"]
+        let file_path = name.filepath(ctx, ext)
+
+        if (!fs.existsSync(file_path) || overwrite) {
+            try {
+                await DownloadFile(dl_link.toString(), file_path)
+            } catch (e) {
+                status = "error"
+            }
         }
-        return download_paths
+        else {
+            status = "existing"
+        }
+
+        return {
+            path: file_path,
+            status
+        };
     }
 
     /**
@@ -30,7 +48,7 @@ class AssetsDownloader{
      * @param textures
      */
     public async downloadTextures(...textures: ResourceName[]) {
-        await this.downloadAsset(textures,"textures",".png")
+        return await Promise.all(textures.map(x=>this.downloadAsset(x, "textures", ".png")))
     }
 }
 
