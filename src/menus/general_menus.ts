4dbfe1853f313c98852c5e39b5065a3a67936fea
ultr42 <ultr42@skiff.com>
2023-08-26 16:51:33 +0800

    proejct restructuring

--- src/menus/general_menus.ts
+++ src/menus/general_menus.ts
@@ -1,66 +1,9 @@
 import chalk from "chalk";
 import ora from 'ora';
 import SummaryManager from "../resources/SummaryManager";
-import {ask_new_item} from "./new_item";
-import {ask_new_block} from "./new_block";
-import {ask_new_texture} from "./new_texture";
-import {ask_new_shader} from "./new_shader";
-
-import { menu_blocks_list} from "./assets_list";
 import {IMenuConfig} from "../prompts/menu";
 import figureSet from "figures";
-
-
-//
-// async function menu_add() {
-//     const answer = await select(
-//         {
-//             message: "Main Menu - New Asset",
-//             choices: [
-//                 {name: "Add Item", value: "new_item"},
-//                 {name: "Add Block", value: "new_block"},
-//                 {name: "Add Texture", value: "new_texture"},
-//                 {name: "Add Shader", value: "new_shader"},
-//                 {name: "Back", value: "back"},
-//             ]
-//         }
-//     )
-//     switch (answer) {
-//         case "new_item":
-//             await ask_new_item()
-//             break;
-//         case "new_block":
-//             await ask_new_block()
-//             break;
-//         case "new_texture":
-//             await ask_new_texture()
-//             break;
-//         case "new_shader":
-//             await ask_new_shader()
-//             break;
-//         case "back":
-//             return
-//     }
-// }
-
-
-export const menu_new_asset: IMenuConfig = {
-    title: "New Asset",
-    options: [
-        {title: "Add Item", custom: ask_new_item},
-        {title: "Add Block", custom: ask_new_block},
-        {title: "Add Texture", custom: ask_new_texture},
-        {title: "Add Shader", custom: ask_new_shader},
-    ]
-}
-
-export const menu_existing_asset: IMenuConfig = {
-    title: "Existing Asset",
-    options: [
-        {title:"Items"},
-        {title:"Blocks", custom: menu_blocks_list}
-    ]
-}
+import {menu_assets} from "./menu_assets";
 
 export const menu_watch: IMenuConfig = {
     title: "Watch",
@@ -87,8 +30,7 @@ export const menu_settings: IMenuConfig = {
 export const menu_start: IMenuConfig = {
     title: "Main Menu",
     options: [
-        menu_new_asset,
-        menu_existing_asset,
+        menu_assets,
         menu_watch,
         menu_settings
     ]
