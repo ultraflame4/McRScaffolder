771d75e4e6a0f5e8dfb8371f20331fd73477a9f2
ultr42 <ultr42@skiff.com>
2023-08-26 18:23:36 +0800

    Manually Lazy loaded ResourcePackModel

--- src/resources/common.ts
+++ src/resources/common.ts
@@ -24,7 +24,7 @@ export class ResourcePackItemAsset implements ITextureAsset {
         if (typeof item_id == "string") item_id = ResourceName.fromString(`item/${item_id}`)
         return new ResourcePackItemAsset(
             item_id,
-             await ResourcePackModel.fromModelId(item_id)
+            await ResourcePackModel.fromModelId(item_id)
         )
     }
 
@@ -45,27 +45,66 @@ export class ResourcePackBlockAsset implements ITextureAsset {
 }
 
 class ResourcePackModel {
-    model_id: ResourceName
-    parent_model_id: ResourceName | null
-    textures: ResourcePackTexture[]
 
-    constructor(model_id: ResourceName, parent_model_id: ResourceName, textures: ResourcePackTexture[]) {
+
+    model_id: ResourceName
+    private _parent_model_id: ResourceName | null
+    private _textures: ResourcePackTexture[]
+    private _loaded: boolean = false
+
+    constructor(model_id: ResourceName, parent_model_id: ResourceName, textures: ResourcePackTexture[], loaded: boolean = false) {
         this.model_id = model_id;
-        this.parent_model_id = parent_model_id;
-        this.textures = textures;
+        this._parent_model_id = parent_model_id;
+        this._textures = textures;
+        this._loaded = loaded
     }
 
-    static async fromModelId(model_id: ResourceName): Promise<ResourcePackModel>{
+
+    get textures(): ResourcePackTexture[] {
+        if (!this._loaded) throw new Error("Attempted to use .textures before data is loaded")
+        return this._textures;
+    }
+
+    get parent_model_id(): ResourceName | null {
+        if (!this._loaded) throw new Error("Attempted to use .parent_model_id before data is loaded")
+        return this._parent_model_id;
+    }
+
+    /**
+     * Call this before using any attributes.
+     *
+     * This is needed because this object will be available in a list with hundreds of other models.
+     * Loading all the models at once takes a long time and results in unresponsive prompts
+     * @param force If true, will reload and re-read data even when not needed.
+     */
+    public async loadData(force: boolean = false) {
+        if (this._loaded && !force) return
+        let data = await SummaryManager.read_model(this.model_id);
+        this._parent_model_id = ResourceName.fromString(data["parent"] as string) ?? null
+        this._textures = Object.entries(data["_textures"] ?? {}).map(([k, v]) => {
+            return {
+                name: k,
+                path: ResourceName.fromString(v as string)
+            }
+        })
+        this._loaded = true
+    }
+
+    /**
+     * Creates an instance from a model_id
+     * @param model_id Id / Resource path to the model
+     * @param force_load When true, immediate loads the data instead of depending on user to load it.
+     */
+
+    static async fromModelId(model_id: ResourceName, force_load:boolean=false): Promise<ResourcePackModel> {
         let data = await SummaryManager.read_model(model_id);
-        return new ResourcePackModel(
+        let o = new ResourcePackModel(
             model_id,
-            ResourceName.fromString(data["parent"] as string) ?? null,
-            Object.entries(data["textures"]??{}).map(([k,v])=>{
-                return {
-                    name: k,
-                    path: ResourceName.fromString(v as string)
-                }
-            })
-        )
+            null,
+            [],
+            false
+        );
+        if (force_load) await o.loadData(true)
+        return o
     }
 }
